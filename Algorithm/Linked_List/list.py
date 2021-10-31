2c6594f8af16b47d82b0ab5dd6e95fd022e6207a
Siyam <42336409+espSiyam@users.noreply.github.com>
2021-10-31 13:34:13 +0600

    linked list

--- Algorithm/Linked_List/list.py
+++ Algorithm/Linked_List/list.py
@@ -7,6 +7,46 @@ class LinkedList:
 	def __init__(self):
 		self.head = None
 
+	def insert_at_begining(self, data):
+		node = Node(data, self.head)
+		self.head = node
+
+	def insert_at_end(self, data):
+		if self.head is None:
+			self.head = Node(data, None)
+			return
+
+		itr = self.head
+		while itr.next:
+			itr.next
+
+		itr.next = Node(data, None)
+
+
+	def insert_values(self, data_list):
+		self.head = None # Wiping out all previous lists
+		for data in data_list:
+			self.insert_at_end(data)
+
+
+	def print(self):
+		if self.head is None:
+			print("Linked list is empty!")
+			return
+
+		itr = self.head
+		llstr = ''
+		while itr:
+			llstr += str(itr.data) + "--->"
+			itr = itr.next
+		print(llstr)
+
+	
+
 
 if __name__ == '__main__':
-	pass
+
+	ll = LinkedList()
+	ll.insert_values(["apple", "banana", "orange", "grapes"])
+	ll.print()
+
